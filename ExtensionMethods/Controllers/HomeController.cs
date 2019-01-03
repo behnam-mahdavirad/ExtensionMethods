3270a094df4da8a93e842e58ddbf70c9398ab16e
Behnam Mahdavirad <behnam.mahdavirad@hotmail.com>
2019-01-03 22:28:27 +0330

    add controller

---
+++ ExtensionMethods/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExtensionMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        } // end ActionResult Index
+
+    } // end public class HomeController
+
+} // end namespace ExtensionMethods.Controllers
