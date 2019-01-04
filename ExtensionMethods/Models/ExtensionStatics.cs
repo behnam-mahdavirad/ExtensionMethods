a371486e7fade63c41120ac711a18afe1fdd5827
Behnam Mahdavirad <behnam.mahdavirad@hotmail.com>
2019-01-04 10:18:54 +0330

    enhancement

--- ExtensionMethods/Models/ExtensionStatics.cs
+++ ExtensionMethods/Models/ExtensionStatics.cs
@@ -7,12 +7,22 @@ namespace ExtensionMethods.Models
 {
     public static class ExtensionStatics
     {
+        /// <summary>
+        /// Check if the input string is Integer or not
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>bool</returns>
         public static bool IsInteger(this string str)
         {
             int output;
             return Int32.TryParse(str, out output);
         } // end method IsNumeric
 
+        /// <summary>
+        /// Count all words in a given string
+        /// </summary>
+        /// <param name="str">string to begin with</param>
+        /// <returns>int</returns>
         public static int CountWords(this string str)
         {
             return str.Split(new char[] { ' ', '.', ',' }).Length;
